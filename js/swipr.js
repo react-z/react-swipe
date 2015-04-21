56ed96d47d5a7cedd6ec02884a6563b16ecb4e4c
jameszmartin <jameszwilliammartin@gmail.com>
2015-04-21 12:18:23 +1000

    Create swipr.js

---
+++ js/swipr.js
@@ -0,0 +1,47 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+/**
+ * React Swipr component
+**/
+
+var ReactSwipr = React.createClass({
+  getInitialState: function(){
+     return {
+     }
+  },
+  componentDidMount: function() {
+
+    var swipr = require('swipr');
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var simple = document.querySelector('.react-swipr');
+        swipr(simple);
+    });
+        
+  },
+  componentWillUnmount: function() {
+  },  
+  slide: function (nextSlide) {
+  },
+  render: function(){
+
+    return (
+
+      React.createElement("div", {className: "react-swipr"}, 
+
+        React.createElement("div", {className: "swipr"}, 
+            React.createElement("ul", {className: "swipr_slides"}, 
+              this.props.children
+            )
+        )
+
+      )
+
+    );
+  }
+
+});
+
+module.exports = ReactSwipr;
