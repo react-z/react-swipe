1565f2d2e8942f31670ce6ebd3d99e7012e381ad
svnm <stevenisekimartin@gmail.com>
2019-04-07 00:53:31 +1100

    prettier Swipe.spec

--- src/__tests__/Swipe.spec.js
+++ src/__tests__/Swipe.spec.js
@@ -6,13 +6,13 @@ configure({ adapter: new Adapter() })
 /* setup jsdom */
 var jsdom = require('jsdom')
 const { JSDOM } = jsdom
-const window = (new JSDOM('')).window
+const window = new JSDOM('').window
 global.window = window
 global.document = window.document
 
 import React from 'react'
 import renderer from 'react-test-renderer'
-import {shallow} from 'enzyme'
+import { shallow } from 'enzyme'
 import Swipe from '../Swipe'
 
 test('Swipe renders correctly and matches snapshot', () => {
@@ -27,12 +27,12 @@ test('Swipe renders correctly and matches snapshot', () => {
       <li>
         <p>more more swiping...</p>
       </li>
-    </Swipe>,
+    </Swipe>
   )
 
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
 
 test('Swipe renders the correct elements', () => {
   const wrapper = shallow(
@@ -46,22 +46,27 @@ test('Swipe renders the correct elements', () => {
       <li>
         <p>more more swiping...</p>
       </li>
-    </Swipe>,
-  );
+    </Swipe>
+  )
 
   expect(wrapper.find('.react-swipe').length).toEqual(1)
   expect(wrapper.find('.swipr_slides').length).toEqual(1)
   expect(wrapper.find('#react-swipe-2').length).toEqual(1)
 
   expect(wrapper.find('li').length).toEqual(3)
-  expect(wrapper.find('p').first().text()).toEqual('swiping...')
+  expect(
+    wrapper
+      .find('p')
+      .first()
+      .text()
+  ).toEqual('swiping...')
 
   expect(wrapper.find('svg').length).toEqual(2)
   expect(wrapper.find('.swipr_next').length).toEqual(1)
   expect(wrapper.find('.swipr_prev').length).toEqual(1)
 
-  wrapper.setProps({navigation: false});
+  wrapper.setProps({ navigation: false })
   expect(wrapper.find('svg').length).toEqual(0)
   expect(wrapper.find('.swipr_next').length).toEqual(0)
   expect(wrapper.find('.swipr_prev').length).toEqual(0)
-});
+})
